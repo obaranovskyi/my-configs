48d9cbf4284a503c57761f827343d8a006060023
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-17 07:25:32 +0200

    Removed ytd due to remote version

--- python/ytd/terminal.py
+++
@@ -1,8 +0,0 @@
-def replace_terminal_encoding(value_to_decode: str):
-    return value_to_decode   \
-        .replace('\\&', '&') \
-        .replace('\\@', '@') \
-        .replace('\\?', '?') \
-        .replace('\\=', '=') \
-        .replace('\\(', '(') \
-        .replace('\\)', ')')
