55404aff7037e3c1478b2045ae1a91412a3eaebb
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-04-13 16:45:20 +0300

    Auto git push ...

--- nvim/plugin-configs/cokeline.lua
+++ nvim/plugin-configs/cokeline.lua
@@ -8,6 +8,152 @@ local bg_color = get_hex('ColorColumn', 'bg')
 local start_icon = ''
 local end_icon = ''
 
+--[[ require('cokeline').setup({ ]]
+--[[       show_if_buffers_are_at_least = 1, ]]
+--[[       mappings = { ]]
+--[[           cycle_prev_next = true ]]
+--[[       }, ]]
+--[[       default_hl = { ]]
+--[[         bg = function(buffer) ]]
+--[[           if buffer.is_focused then ]]
+--[[             return active_bg_color ]]
+--[[           end ]]
+--[[         end, ]]
+--[[       }, ]]
+--[[       components = { ]]
+--[[           { ]]
+--[[             text = function(buffer) ]]
+--[[               local _text = '' ]]
+--[[               if buffer.index > 1 then _text = ' ' end ]]
+--[[               if buffer.is_focused or buffer.is_first then ]]
+--[[                 _text = _text .. start_icon ]]
+--[[               end ]]
+--[[               return _text ]]
+--[[             end, ]]
+--[[             fg = function(buffer) ]]
+--[[               if buffer.is_focused then ]]
+--[[                 return active_bg_color ]]
+--[[               elseif buffer.is_first then ]]
+--[[                 return inactive_bg_color ]]
+--[[               end ]]
+--[[             end, ]]
+--[[             bg = function(buffer) ]]
+--[[               if buffer.is_focused then ]]
+--[[                 if buffer.is_first then ]]
+--[[                   return bg_color ]]
+--[[                 else ]]
+--[[                   return inactive_bg_color ]]
+--[[                 end ]]
+--[[               elseif buffer.is_first then ]]
+--[[                   return bg_color ]]
+--[[               end ]]
+--[[             end ]]
+--[[           }, ]]
+--[[           { ]]
+--[[               text = function(buffer) ]]
+--[[                   local status = '' ]]
+--[[                   if buffer.is_readonly then ]]
+--[[                       status = '➖' ]]
+--[[                   elseif buffer.is_modified then ]]
+--[[                       status = '' ]]
+--[[                   end ]]
+--[[                   return status ]]
+--[[               end, ]]
+--[[           }, ]]
+--[[           { ]]
+--[[               text = function(buffer) ]]
+--[[                   return " " .. buffer.devicon.icon ]]
+--[[               end, ]]
+--[[               fg = function(buffer) ]]
+--[[                 if buffer.is_focused then ]]
+--[[                   return buffer.devicon.color ]]
+--[[                 end ]]
+--[[               end ]]
+--[[           }, ]]
+--[[           { ]]
+--[[               text = function(buffer) ]]
+--[[                 return buffer.unique_prefix .. buffer.filename ]]
+--[[               end, ]]
+--[[               fg = function(buffer) ]]
+--[[                   if(buffer.diagnostics.errors > 0) then ]]
+--[[                       return '#910101' ]]
+--[[                   end ]]
+--[[               end, ]]
+--[[               style = function(buffer) ]]
+--[[                   local text_style = 'NONE' ]]
+--[[                   if buffer.is_focused then ]]
+--[[                       text_style = 'bold' ]]
+--[[                   end ]]
+--[[                   if buffer.diagnostics.errors > 0 then ]]
+--[[                       if text_style ~= 'NONE' then ]]
+--[[                           text_style = text_style .. ',underline' ]]
+--[[                       else ]]
+--[[                           text_style = 'underline' ]]
+--[[                       end ]]
+--[[                   end ]]
+--[[                   return text_style ]]
+--[[               end ]]
+--[[           }, ]]
+--[[           { ]]
+--[[               text = function(buffer) ]]
+--[[                   local errors = buffer.diagnostics.errors ]]
+--[[                   if(errors <= 9) then ]]
+--[[                       errors = ' ' .. '' ]]
+--[[                   else ]]
+--[[                       errors = "🙃" ]]
+--[[                   end ]]
+--[[                   return errors .. ' ' ]]
+--[[               end, ]]
+--[[               fg = function(buffer) ]]
+--[[                 if buffer.diagnostics.errors == 0 then ]]
+--[[                   return '#3DEB63' ]]
+--[[                 elseif buffer.diagnostics.errors <= 9 then ]]
+--[[                   return '#DB121B' ]]
+--[[                 end ]]
+--[[               end ]]
+--[[           }, ]]
+--[[           { ]]
+--[[               text = '', ]]
+--[[               delete_buffer_on_left_click = true ]]
+--[[           }, ]]
+--[[           { ]]
+--[[             text = function(buffer) ]]
+--[[               if buffer.is_focused or buffer.is_last then ]]
+--[[                 return end_icon ]]
+--[[               else ]]
+--[[                 return ' ' ]]
+--[[               end ]]
+--[[             end, ]]
+--[[             fg = function(buffer) ]]
+--[[               if buffer.is_focused then ]]
+--[[                 return active_bg_color ]]
+--[[               elseif buffer.is_last then ]]
+--[[                 return inactive_bg_color ]]
+--[[               else ]]
+--[[                 return bg_color ]]
+--[[               end ]]
+--[[             end, ]]
+--[[             bg = function(buffer) ]]
+--[[               if buffer.is_focused then ]]
+--[[                 if buffer.is_last then ]]
+--[[                   return bg_color ]]
+--[[                 else ]]
+--[[                   return inactive_bg_color ]]
+--[[                 end ]]
+--[[               elseif buffer.is_last then ]]
+--[[                   return bg_color ]]
+--[[               end ]]
+--[[             end ]]
+--[[           } ]]
+--[[       }, ]]
+--[[   }) ]]
+
+
+
+local get_hex = require("cokeline.utils").get_hex
+local active_bg_color = '#4c99e0'
+local inactive_bg_color = get_hex('Normal', 'bg')
+local bg_color = get_hex('ColorColumn', 'bg')
 require('cokeline').setup({
       show_if_buffers_are_at_least = 1,
       mappings = {
@@ -26,7 +172,7 @@ require('cokeline').setup({
               local _text = ''
               if buffer.index > 1 then _text = ' ' end
               if buffer.is_focused or buffer.is_first then
-                _text = _text .. start_icon
+                _text = _text .. ''
               end
               return _text
             end,
@@ -76,7 +222,7 @@ require('cokeline').setup({
               end,
               fg = function(buffer)
                   if(buffer.diagnostics.errors > 0) then
-                      return '#910101'
+                      return '#C95157'
                   end
               end,
               style = function(buffer)
@@ -98,7 +244,7 @@ require('cokeline').setup({
               text = function(buffer)
                   local errors = buffer.diagnostics.errors
                   if(errors <= 9) then
-                      errors = ' ' .. ''
+                      errors = ' '
                   else
                       errors = "🙃"
                   end
@@ -119,7 +265,7 @@ require('cokeline').setup({
           {
             text = function(buffer)
               if buffer.is_focused or buffer.is_last then
-                return end_icon
+                return ''
               else
                 return ' '
               end
