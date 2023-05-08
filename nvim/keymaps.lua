cc8bdf41ac00ff0b44fe00198164bfef58a11957
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-08 17:00:57 +0300

    Auto git push ...

--- nvim/keymaps.lua
+++ nvim/keymaps.lua
@@ -114,6 +114,20 @@ keymap("n", "gw", ":cprev<CR>", opts)
 keymap("n", "gb", ":cfirst<CR>", opts)
 keymap("n", "gl", ":clast<CR>", opts)
 keymap("n", "gx", ":cclose<CR>", opts)
+vim.cmd [[
+  " When using `dd` in the quickfix list, remove the item from the quickfix list.
+  function! RemoveQFItem()
+    let curqfidx = line('.') - 1
+    let qfall = getqflist()
+    call remove(qfall, curqfidx)
+    call setqflist(qfall, 'r')
+    execute curqfidx + 1 . "cfirst"
+    :copen
+  endfunction
+  :command! RemoveQFItem :call RemoveQFItem()
+  " Use map <buffer> to only map dd in the quickfix window. Requires +localmap
+  autocmd FileType qf map <buffer> dd :RemoveQFItem<cr>
+]]
 
 -- ------------------------------------------------
 -- Command line mode navigation mappings
