4476f685e6d792c5d95a5d897519a9cda38bd1d9
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-13 10:28:12 +0300

    Auto git push ...

--- zsh/custom-aliases/cs.sh
+++ zsh/custom-aliases/cs.sh
@@ -1,11 +1,41 @@
+repo_name='obaranovskyi/cheatsheet'
+url='https://github.com'
+raw_url='https://raw.githubusercontent.com'
+main_src='main/src'
+
+
 function cheatsheet() {
-  local value=$(curl -s "https://raw.githubusercontent.com/obaranovskyi/cheatsheet/main/src/$1.md")
-  echo "$value" | bat -l 'markdown' -n
+  local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
+  value=$(echo $value | sed -e 's/404: Not Found//g')
+
+  if [ ! -z "$value" ]
+  then
+    if ! command -v bat &> /dev/null
+    then
+      echo "$value"
+    else
+      echo "$value" | bat -l 'markdown' -n
+    fi
+  fi
+}
+
+function cheatsheet_list() {
+  local value=$(
+    curl -s "${url}/${repo_name}/tree/${main_src}" | \
+    grep '>.*\.md<'                                | \
+    sed -e 's/<[^>]*>//g'                          | \
+    sed -e 's/\.md//g'                             | \
+    awk '{print $1}'
+  )
+  echo "$value"
 }
 
 alias cs="cheatsheet"
+alias csl="cheatsheet_list"
+
 
 cs-help() {
     echo "\n${COLOR_YELLOW}  Cheatsheet:";
     echo "${COLOR_BLUE}  cs <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name>"
+    echo "${COLOR_BLUE}  csl ${COLOR_GREEN}- display the list of cheat sheets that can be searched for"
 }
