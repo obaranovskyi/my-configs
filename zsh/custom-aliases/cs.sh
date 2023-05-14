fcc4573e5360e5e46314ab250f931ee8c1a0b62b
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-14 07:06:50 +0300

    Auto git push ...

--- zsh/custom-aliases/cs.sh
+++ zsh/custom-aliases/cs.sh
@@ -3,7 +3,6 @@ url='https://github.com'
 raw_url='https://raw.githubusercontent.com'
 main_src='main/src'
 
-
 function cheatsheet() {
   local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
 
@@ -15,18 +14,11 @@ function cheatsheet() {
       echo "$value" | bat -l 'markdown' -n
     fi
   fi
-
 }
 
 function cheatsheet_list() {
-  local value=$(
-    curl -s "${url}/${repo_name}/tree/${main_src}" | \
-    grep '>.*\.md<'                                | \
-    sed -e 's/<[^>]*>//g'                          | \
-    sed -e 's/\.md//g'                             | \
-    awk '{print $1}'
-  )
-  echo "$value"
+  local values=$(curl -s "${raw_url}/${repo_name}/main/content.md")
+  echo $values
 }
 
 alias cs="cheatsheet"
