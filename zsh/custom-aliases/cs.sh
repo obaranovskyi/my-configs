8f7321cb0e9905cac824015087386dcae5e21356
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-14 07:36:01 +0300

    Auto git push ...

--- zsh/custom-aliases/cs.sh
+++ zsh/custom-aliases/cs.sh
@@ -3,16 +3,31 @@ url='https://github.com'
 raw_url='https://raw.githubusercontent.com'
 main_src='main/src'
 
-function cheatsheet() {
+function cs() {
   local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
-
   if [[ ! $value == *"404: Not Found"* ]]; then
-    if ! command -v bat &> /dev/null
-    then
-      echo "$value" | less
-    else
-      echo "$value" | bat -l 'markdown' -n
-    fi
+    echo $value
+  fi
+}
+
+function csbat() {
+  local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
+  if [[ ! $value == *"404: Not Found"* ]]; then
+    echo "$value" | bat -l 'markdown' -n
+  fi
+}
+
+function csglow() {
+  local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
+  if [[ ! $value == *"404: Not Found"* ]]; then
+    echo $value | glow
+  fi
+}
+
+function csless() {
+  local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
+  if [[ ! $value == *"404: Not Found"* ]]; then
+    echo $value | less
   fi
 }
 
@@ -21,12 +36,12 @@ function cheatsheet_list() {
   echo $values
 }
 
-alias cs="cheatsheet"
-alias csl="cheatsheet_list"
-
 
 cs-help() {
     echo "\n${COLOR_YELLOW}  Cheatsheet:";
     echo "${COLOR_BLUE}  cs <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name>"
+    echo "${COLOR_BLUE}  csbat <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using bat app"
+    echo "${COLOR_BLUE}  csglow <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using glow app"
+    echo "${COLOR_BLUE}  csless <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using less app"
     echo "${COLOR_BLUE}  csl ${COLOR_GREEN}- display the list of cheat sheets that can be searched for"
 }
