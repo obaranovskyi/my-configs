423a57d719ce1210ecfc024c0a34595d9e05ba49
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-15 07:46:40 +0300

    Auto git push ...

--- zsh/custom-aliases/cs.sh
+++ zsh/custom-aliases/cs.sh
@@ -10,28 +10,28 @@ function cs() {
   fi
 }
 
-function csbat() {
+function csb() {
   local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
   if [[ ! $value == *"404: Not Found"* ]]; then
     printf "$value" | bat -l 'markdown' -n
   fi
 }
 
-function csglow() {
+function csg() {
   local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
   if [[ ! $value == *"404: Not Found"* ]]; then
     printf "$value" | glow
   fi
 }
 
-function csless() {
+function csl() {
   local value=$(curl -s "${raw_url}/${repo_name}/${main_src}/$1.md")
   if [[ ! $value == *"404: Not Found"* ]]; then
     printf "$value" | less
   fi
 }
 
-function csl() {
+function cslist() {
   printf "$(curl -s "${raw_url}/${repo_name}/main/content.md")"
 }
 
@@ -39,8 +39,8 @@ function csl() {
 cs-help() {
     echo "\n${COLOR_YELLOW}  Cheatsheet:";
     echo "${COLOR_BLUE}  cs <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name>"
-    echo "${COLOR_BLUE}  csbat <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using bat app"
-    echo "${COLOR_BLUE}  csglow <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using glow app"
-    echo "${COLOR_BLUE}  csless <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using less app"
-    echo "${COLOR_BLUE}  csl ${COLOR_GREEN}- display the list of cheat sheets that can be searched for"
+    echo "${COLOR_BLUE}  csb <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using bat app"
+    echo "${COLOR_BLUE}  csg <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using glow app"
+    echo "${COLOR_BLUE}  csl <app-name> ${COLOR_GREEN}- display cheatsheet for <app-name> using less app"
+    echo "${COLOR_BLUE}  cslist ${COLOR_GREEN}- display the list of cheat sheets that can be searched for"
 }
