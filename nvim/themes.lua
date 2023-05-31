65efc594008afb6c0ba887dcb156d7908d650400
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-05-31 15:43:27 +0300

    Auto git push ...

--- nvim/themes.lua
+++ nvim/themes.lua
@@ -4,18 +4,18 @@ vim.opt.syntax = "enable"
 -- vim.cmd "colorscheme tokyonight-moon"
 -- vim.cmd "colorscheme catppuccin"
 -- vim.cmd "colorscheme catppuccin-macchiato"
--- vim.cmd "colorscheme sonokai"
-
+-- -- vim.cmd "colorscheme sonokai"
+   -- 
 -- vim.cmd('colorscheme dracula')
 
 -- vim.opt.termguicolors = true
 -- vim.cmd("colorscheme melange")
 
--- vim.opt.termguicolors = true
--- vim.cmd "colorscheme onedark"
+vim.opt.termguicolors = true
+vim.cmd "colorscheme onedark"
 
-vim.cmd "colorscheme material"
-vim.api.nvim_set_hl(0, "Cursor", {fg='#cc9900', bg='#919191'})
+-- vim.cmd "colorscheme material"
+-- vim.api.nvim_set_hl(0, "Cursor", {fg='#cc9900', bg='#919191'})
 
 -- vim.cmd 'colorscheme palenight'
 -- vim.g.palenight_terminal_italics = 1
