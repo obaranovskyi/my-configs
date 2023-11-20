302abb07b174a052b8fdca017c9f0b77898c5bab
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-11-20 10:30:48 +0200

    Auto git push ...

--- nvim/plugin-configs/luasnip.lua
+++ nvim/plugin-configs/luasnip.lua
@@ -12,3 +12,25 @@ vim.keymap.set({ "i", "s" }, "<C-E>", function()
   end
 end, { silent = true })
 
+
+
+
+
+
+-- ISSUE FIX: always use tab to expand the text 
+-- https://www.reddit.com/r/neovim/comments/15ptyvb/nvim_cmp_super_tab_goes_crazy/
+local unlink_group = vim.api.nvim_create_augroup('UnlinkSnippet', { })
+vim.api.nvim_create_autocmd('ModeChanged', {
+   group = unlink_group,
+   -- when going from select mode to normal and when leaving insert mode
+   pattern = { 's:n', 'i:*' },
+   callback = function(event)
+     if
+       ls.session
+       and ls.session.current_nodes[event.buf]
+       and not ls.session.jump_active
+     then
+       ls.unlink_current()
+     end
+   end,
+})
