6e2e767092f4dc9cbd02a9b4b7baf61f9803235f
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-12-08 16:57:08 +0200

    Auto git push ...

--- nvim/lsp/cmp.lua
+++ nvim/lsp/cmp.lua
@@ -55,36 +55,36 @@ cmp.setup({
     ["<Right>"] = cmp.mapping.confirm({ select = true }),
 
     -- -- Config-2 luaship (right now works without it)
-    -- ["<Tab>"] = cmp.mapping(function(fallback
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expandable() then
-    --     luasnip.expand()
-    --   elseif luasnip.expand_or_jumpable() then
-    --     luasnip.expand_or_jump()
-    --   -- I was fixing the issue with check_backspace
-    --   -- Right now works without it 
-    --   --[[ elseif check_backspace() then ]]
-    --   --[[   fallback() ]]
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   "i",
-    --   "s",
-    -- }),
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   "i",
-    --   "s",
-    -- }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      -- I was fixing the issue with check_backspace
+      -- Right now works without it 
+      --[[ elseif check_backspace() then ]]
+      --[[   fallback() ]]
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
   }),
 
   sources = cmp.config.sources({
