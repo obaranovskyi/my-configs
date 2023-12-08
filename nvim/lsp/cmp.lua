c222bb484157cc74ff17c6df29b4b01698139b16
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-12-08 17:08:19 +0200

    Auto git push ...

--- nvim/lsp/cmp.lua
+++ nvim/lsp/cmp.lua
@@ -8,6 +8,11 @@ if not snip_status_ok then
   return
 end
 
+-- local check_backspace = function()
+--   local col = vim.fn.col "." - 1
+--   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+-- end
+
 local source_mapping = {
   buffer = "[Buf]",
   nvim_lsp = "[LSP]",
