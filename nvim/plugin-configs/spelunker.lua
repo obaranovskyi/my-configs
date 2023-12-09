f1aa2dfd568a7430d0e217bbfde02e8672551f23
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2023-12-09 10:28:57 +0200

    Auto git push ...

--- nvim/plugin-configs/spelunker.lua
+++ nvim/plugin-configs/spelunker.lua
@@ -1,8 +1,11 @@
 vim.cmd [[
+  set nospell
+  let g:enable_spelunker_vim = 1
   let g:spelunker_target_min_char_len = 3
-  " Override highlight setting.
-  highlight SpelunkerSpellBad cterm=underline ctermfg=247 gui=underline guifg=#e64866
-  highlight SpelunkerComplexOrCompoundWord cterm=underline ctermfg=NONE gui=underline guifg=NONE
+
+  " " Override highlight setting.
+  " highlight SpelunkerSpellBad cterm=underline ctermfg=247 gui=underline guifg=#e64866
+  " highlight SpelunkerComplexOrCompoundWord cterm=underline ctermfg=NONE gui=underline guifg=NONE
 
   let g:spelunker_max_hi_words_each_buf = 100
 ]]
