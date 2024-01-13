c0a92805aa6be18973f7aa89f06f61a62e743f43
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-01-13 21:27:05 +0200

    Auto git push ...

--- neovim/configs/search.lua
+++ neovim/configs/search.lua
@@ -1,17 +1,18 @@
 -- [Single file mode]
-local left9 = string.rep('<Left>', 9)
-local left10 = string.rep('<Left>', 10)
+local left = function(repeat_count)
+  return string.rep('<Left>', repeat_count)
+end
 local rPrefix = "<leader>r"
 local subs = ":%s/<C-r>h//g"
 
-vim.keymap.set("v", rPrefix .. "f", '"hy' .. subs .. 'c | :noh' .. left10)
-vim.keymap.set("v", rPrefix .. "t", '"hy' .. subs .. ' | :noh' .. left9)
+vim.keymap.set("v", rPrefix .. "f", '"hy' .. subs .. 'c | :noh' .. left(10))
+vim.keymap.set("v", rPrefix .. "t", '"hy' .. subs .. ' | :noh' .. left(9))
 
-vim.keymap.set("n", rPrefix .. "f", '"hyiw' .. subs .. 'c | :noh' .. left10)
-vim.keymap.set("n", rPrefix .. "t", '"hyiw' .. subs .. ' | :noh' .. left9)
+vim.keymap.set("n", rPrefix .. "f", '"hyiw' .. subs .. 'c | :noh' .. left(10))
+vim.keymap.set("n", rPrefix .. "t", '"hyiw' .. subs .. ' | :noh' .. left(9))
 
-vim.keymap.set("n", rPrefix .. "F", '"hyiW' .. subs .. 'c | :noh' .. left10)
-vim.keymap.set("n", rPrefix .. "T", '"hyiW' .. subs .. ' | :noh' .. left9)
+vim.keymap.set("n", rPrefix .. "F", '"hyiW' .. subs .. 'c | :noh' .. left(10))
+vim.keymap.set("n", rPrefix .. "T", '"hyiW' .. subs .. ' | :noh' .. left(9))
 
 -- INFO: This has to be investigated
 vim.cmd [[
