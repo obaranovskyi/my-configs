fadca960664e9e89df280baa737c4340786121d2
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-07 13:27:08 +0300

    Auto git push ...

--- neovim/plugins/navigation/leap.lua
+++ neovim/plugins/navigation/leap.lua
@@ -1,26 +1,25 @@
 return {
-  'ggandor/leap.nvim',
-  dependencies = {
-    "tpope/vim-repeat"
-  },
-  config = function()
-    vim.keymap.set('n', 's', function()
-      local focusable_windows = vim.tbl_filter(
-        function(win) return vim.api.nvim_win_get_config(win).focusable end,
-        vim.api.nvim_tabpage_list_wins(0)
-      )
-      require('leap').leap { 
-        target_windows = focusable_windows,
-        opts = {
-          -- Don't just to the first match
-          safe_labels = {}
-        }
-      }
+	"ggandor/leap.nvim",
+	dependencies = {
+		"tpope/vim-repeat",
+	},
+	config = function()
+		vim.keymap.set("n", "s", function()
+			local focusable_windows = vim.tbl_filter(function(win)
+				return vim.api.nvim_win_get_config(win).focusable
+			end, vim.api.nvim_tabpage_list_wins(0))
 
-      -- Gray color for the rest
-      vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
-    end)
+			require("leap").leap({
+				target_windows = focusable_windows,
+				opts = {
+					-- Don't just to the first match
+					safe_labels = {},
+				},
+			})
 
-  end,
-  lazy = false,
+			-- Gray color for the rest
+			vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+		end)
+	end,
+	lazy = false,
 }
