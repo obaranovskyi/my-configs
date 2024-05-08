0d0667516d3f54b9c1d157f30e93896f50ec8003
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-05-08 17:14:09 +0300

    Auto git push ...

--- neovim/plugins/code/conform.lua
+++ neovim/plugins/code/conform.lua
@@ -1,60 +1,61 @@
 return {
-  'stevearc/conform.nvim',
-  config = function()
-    local conform = require('conform')
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        json = { "biome" },
-      },
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				json = { "biome" },
+				markdown = { "mdformat" },
+			},
 
-      -- Auto format
-      format_on_save = function(bufnr)
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { lsp_fallback = true }
-      end,
-    })
+			-- Auto format
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { lsp_fallback = true }
+			end,
+		})
 
-    -- :LSPInstall on the file you want to add language server
-    -- For instance: json ("biome")
+		-- :LSPInstall on the file you want to add language server
+		-- For instance: json ("biome")
 
-    vim.api.nvim_create_user_command("Format", function(args)
-      local range = nil
-      if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = {
-          start = { args.line1, 0 },
-          ["end"] = { args.line2, end_line:len() },
-        }
-      end
-      conform.format({ async = true, lsp_fallback = true, range = range })
-    end, { range = true })
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			conform.format({ async = true, lsp_fallback = true, range = range })
+		end, { range = true })
 
-    vim.keymap.set("n", "<leader>fd", ':Format<CR>', { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>fd", ":Format<CR>", { noremap = true, silent = true })
 
-    -- INFO: Disable format
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
-  end
+		-- INFO: Disable format
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
 }
