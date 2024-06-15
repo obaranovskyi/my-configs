e38e29a2ecab5007919179b122a0731699d7dd8a
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-06-15 12:38:08 +0300

    Auto git push ...

--- neovim/plugins/navigation/leap.lua
+++ neovim/plugins/navigation/leap.lua
@@ -5,21 +5,27 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "s", function()
-			local focusable_windows = vim.tbl_filter(function(win)
-				return vim.api.nvim_win_get_config(win).focusable
-			end, vim.api.nvim_tabpage_list_wins(0))
-
 			require("leap").leap({
-				target_windows = focusable_windows,
+				-- jump through all the windows
+				target_windows = require("leap.user").get_focusable_windows(),
 				opts = {
-					-- Don't just to the first match
+					-- Don't jump to the first match
 					safe_labels = {},
 				},
 			})
-
-			-- Gray color for the rest
-			vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
 		end)
+
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			callback = function()
+				vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+				vim.api.nvim_set_hl(0, "LeapLabelPrimary", {
+					bg = "gold",
+					fg = "black",
+					bold = false,
+					nocombine = true,
+				})
+			end,
+		})
 	end,
 	lazy = false,
 }
