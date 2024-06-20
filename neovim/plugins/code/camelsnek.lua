bba82143eec34296c16bbb3bad3e9545bdea10fd
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-06-20 07:32:36 +0300

    Auto git push ...

--- neovim/plugins/code/camelsnek.lua
+++ neovim/plugins/code/camelsnek.lua
@@ -1,19 +1,19 @@
 return {
- 'nicwest/vim-camelsnek',
-  config = function()
-    local opts = { noremap = true, silent = true }
-    local keymap = vim.api.nvim_set_keymap
+	"nicwest/vim-camelsnek",
+	config = function()
+		local opts = { noremap = true, silent = true }
+		local keymap = vim.api.nvim_set_keymap
 
-    keymap("v", "<leader>cs", ":Snek<CR>", opts)
-    keymap("v", "<leader>cS", ":Kebab<CR>", opts)
-    keymap("v", "<leader>cc", ":CamelB<CR>", opts)
-    keymap("v", "<leader>cp", ":Camel<CR>", opts)
-    keymap("v", "<leader>cu", ":Screm<CR>", opts)
+		keymap("v", "<leader>cs", ":Snek<CR>", opts)
+		keymap("v", "<leader>cS", ":Kebab<CR>", opts)
+		keymap("v", "<leader>cC", ":CamelB<CR>", opts)
+		keymap("v", "<leader>cp", ":Camel<CR>", opts)
+		keymap("v", "<leader>cu", ":Screm<CR>", opts)
 
-    keymap("n", "<leader>cs", ":Snek<CR>", opts)
-    keymap("n", "<leader>cS", ":Kebab<CR>", opts)
-    keymap("n", "<leader>cc", ":CamelB<CR>", opts)
-    keymap("n", "<leader>cp", ":Camel<CR>", opts)
-    keymap("n", "<leader>cu", ":Screm<CR>", opts)
-  end
+		keymap("n", "<leader>cs", ":Snek<CR>", opts)
+		keymap("n", "<leader>cS", ":Kebab<CR>", opts)
+		keymap("n", "<leader>cC", ":CamelB<CR>", opts)
+		keymap("n", "<leader>cp", ":Camel<CR>", opts)
+		keymap("n", "<leader>cu", ":Screm<CR>", opts)
+	end,
 }
---
+++ neovim/plugins/copilot-chat.lua
@@ -0,0 +1,56 @@
+return {
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		config = function()
+			local chat = require("CopilotChat")
+
+			chat.setup({
+				debug = true,
+				-- default mappings
+				mappings = {
+					complete = {
+						detail = "Use @<Tab> or /<Tab> for options.",
+						insert = "<C-e>", -- If i set this hotkey <Tab> will work as expected
+					},
+					close = {
+						normal = "q",
+						insert = "<C-c>",
+					},
+					reset = {
+						normal = "<C-R>",
+						insert = "<C-R>",
+					},
+					submit_prompt = {
+						normal = "<CR>",
+						insert = "<CR>",
+					},
+					accept_diff = {
+						normal = "<C-y>",
+						insert = "<C-y>",
+					},
+					yank_diff = {
+						normal = "gy",
+					},
+					show_diff = {
+						normal = "gd",
+					},
+					show_system_prompt = {
+						normal = "gp",
+					},
+					show_user_selection = {
+						normal = "gs",
+					},
+				},
+			})
+
+			local opts = { noremap = true, silent = false }
+			local keymap = vim.api.nvim_set_keymap
+			vim.keymap.set({ "n", "v" }, "<leader>cc", ':lua require("CopilotChat").toggle()<CR>', opts)
+		end,
+	},
+}
