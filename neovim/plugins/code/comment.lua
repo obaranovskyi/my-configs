9a87b886cf0069ada09ecf5365b4d3f25c15059c
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-06-27 21:30:55 +0300

    Auto git push ...

--- neovim/plugins/code/comment.lua
+++ neovim/plugins/code/comment.lua
@@ -1,8 +1,13 @@
 return {
-  'numToStr/Comment.nvim',
-  config = function()
-    require('Comment').setup {
-      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    }
-  end
+	"numToStr/Comment.nvim",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		local comment = require("Comment")
+		local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+		comment.setup({
+			pre_hook = ts_context_commentstring.create_pre_hook(),
+		})
+	end,
 }
