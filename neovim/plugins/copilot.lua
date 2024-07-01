9f0d72e19248db81d02748ffbf897a1c39da1fa9
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-07-01 17:21:46 +0300

    Auto git push ...

--- neovim/plugins/copilot.lua
+++ neovim/plugins/copilot.lua
@@ -1,5 +1,12 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		require("copilot").setup()
+
+		vim.keymap.set({ "i" }, "<C-p>", "<Plug>(copilot-previous)", { silent = true })
+		vim.keymap.set({ "i" }, "<C-n>", "<Plug>(copilot-next)", { silent = true })
+		vim.keymap.set("i", "<C-l>", "<Plug>(copilot-accept-word)", { silent = true })
+	end,
 }
 
 -- NOTE: If you see copilot virtual text and suggestion click `C-e` to hide
