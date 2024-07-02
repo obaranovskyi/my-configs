976ef01c1e9e73beda29db30f76cedfa113bc081
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-07-02 18:23:01 +0300

    Auto git push ...

--- neovim/plugins/markdown-preview.lua
+++ neovim/plugins/markdown-preview.lua
@@ -1,25 +1,26 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function() vim.fn["mkdp#util#install"]() end,
-  config = function()
-    local opts = { noremap = true, silent = true }
-    local keymap = vim.api.nvim_set_keymap
-    keymap("n", "<C-s>", ":MarkdownPreview<CR>", opts)
-
-    vim.g.markdown_fenced_languages = {
-      'html',
-      'css',
-      'scss',
-      'python',
-      'bash=sh',
-      'sql',
-      'javascript',
-      'typescript',
-      'lua'
-    }
-
-    vim.g.mkdp_theme = 'light'
-  end
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	ft = { "markdown" },
+	config = function()
+		local opts = { noremap = true, silent = true }
+		local keymap = vim.api.nvim_set_keymap
+		keymap("n", "<leader>sm", "<Plug>MarkdownPreview", opts)
+		vim.g.markdown_fenced_languages = {
+			"html",
+			"css",
+			"scss",
+			"python",
+			"bash=sh",
+			"sql",
+			"javascript",
+			"typescript",
+			"lua",
+		}
+		vim.g.mkdp_theme = "light"
+	end,
 }
