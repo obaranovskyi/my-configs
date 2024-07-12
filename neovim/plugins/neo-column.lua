eb2d29c8337a878d53f64b9e13142c458c395ac2
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-07-12 10:27:04 +0300

    Auto git push ...

--- neovim/plugins/neo-column.lua
+++ neovim/plugins/neo-column.lua
@@ -1,13 +1,14 @@
 return {
-  'ecthelionvi/NeoColumn.nvim',
-  config = function()
-    require('NeoColumn').setup({
-      fg_color = "transparent",
-      bg_color = "#4287f5",
-      NeoColumn = "120",
-      always_on = true,
-      custom_NeoColumn = {},
-      excluded_ft = {},
-    })
-  end
+	"ecthelionvi/NeoColumn.nvim",
+	config = function()
+		require("NeoColumn").setup({
+			fg_color = "transparent",
+			-- bg_color = "#4287f5",
+			bg_color = "#2a4259",
+			NeoColumn = "120",
+			always_on = true,
+			custom_NeoColumn = {},
+			excluded_ft = {},
+		})
+	end,
 }
--- neovim/plugins/ui/image.lua
+++ neovim/plugins/ui/image.lua
@@ -14,6 +14,19 @@ return {
 		},
 	},
 	config = function()
-		require("image").setup({})
+		require("image").setup({
+			backend = "kitty",
+			integrations = {
+				markdown = {
+					enabled = true,
+					clear_in_insert_mode = false,
+					download_remote_images = true,
+					only_render_image_at_cursor = true,
+					filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+				},
+			},
+			tmux_show_only_in_active_window = true,
+			hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif", "*.svg" },
+		})
 	end,
 }
