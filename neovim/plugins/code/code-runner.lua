2d6e4efb2a716ab79b53aae446a8a12f11bb226f
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-07-15 17:01:03 +0300

    Auto git push ...

--- neovim/plugins/code/code-runner.lua
+++ neovim/plugins/code/code-runner.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("code_runner").setup()
 
-		vim.keymap.set("n", "<leader>ra", ":RunCode<CR>")
+		vim.keymap.set("n", "<leader>ra", ":RunClose<CR>:RunCode<CR>")
 		vim.keymap.set("n", "<leader>rs", ":RunClose<CR>")
 	end,
 }
