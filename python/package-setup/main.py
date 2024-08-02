b4b9291562d5ee90e05ff4708316e243eabf181f
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-08-02 19:03:26 +0300

    Auto git push ...

--- python/package-setup/main.py
+++ python/package-setup/main.py
@@ -1,47 +1,35 @@
+"""
+This script reads all requirements.txt files in the parent directory and installs all dependencies.
+"""
 import os
 
-dependencies = [
-    # link-to-markdown
-    "html2text",
-    "requests",
-    # clipboard-tracker
-    "clipboard==0.0.4",
-    "pillow==10.3.0",
-    "pyperclip==1.9.0",
-    # nato-alphabet
-    "rich",
-    # youtube-downloader
-    "certifi",
-    "charset-normalizer",
-    "decorator",
-    "idna",
-    "imageio",
-    "imageio-ffmpeg",
-    "markdown-it-py",
-    "mdurl",
-    "moviepy",
-    "numpy",
-    "Pillow",
-    "proglog",
-    "Pygments",
-    "pytube", # has issue: https://github.com/pytube/pytube/issues/1973
-    "pytubefix", # alternative
-    "requests",
-    "scrapetube",
-    "tqdm",
-    "typing_extensions",
-    "urllib3",
-    "uuid",
-    "rich",
-    # english-assist
-    "googletrans==3.1.0a0",
-    "uuid",
-    "rich",
-    "reportlab",
-    "pydub",
-    "gtts",
-    "genanki",
-    "markdown",
-]
 
-os.system(f"pip install {' '.join(dependencies)} --break-system-packages")
+def get_parent_directory():
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def read_requirements_from_path(requirements_path):
+    if not os.path.isfile(requirements_path):
+        return set()
+    with open(requirements_path, 'r') as file:
+        return set(file.read().splitlines())
+    
+
+def read_requirements():
+    base_path = get_parent_directory()
+    packages_set = set()
+    
+    # List all entries in the base path
+    for entry in os.listdir(base_path):
+        folder_path = os.path.join(base_path, entry)
+        
+        # Check if it's a directory
+        if os.path.isdir(folder_path):
+            requirements_path = os.path.join(folder_path, 'requirements.txt')
+            packages_set.update(read_requirements_from_path(requirements_path))
+    
+    return packages_set
+
+
+if __name__ == '__main__':
+    dependencies = read_requirements()
+    os.system(f"pip install {' '.join(dependencies)} --break-system-packages")
