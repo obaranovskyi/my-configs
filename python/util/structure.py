6cdd7212a8c70caa576aca4848d269e6c9ce4967
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-08-07 14:03:14 +0300

    Auto git push ...

--- python/util/structure.py
+++ python/util/structure.py
@@ -3,7 +3,15 @@ import os
 
 def create_structure(path: str, skip_file_creation=False) -> None:
     """
-    Create the path if it doesn't exist.
+    Create the directory structure specified by the given path. If the path ends with a file name, 
+    the file will be created unless `skip_file_creation` is True.
+
+    Args:
+        path (str): The path to create. If it ends with a file name, the file will be created.
+        skip_file_creation (bool): If True, the file will not be created even if the path ends with a file name.
+
+    Returns:
+        None
     """
     path = path.lstrip('/')
     path = os.path.normpath(path)
@@ -20,7 +28,13 @@ def create_structure(path: str, skip_file_creation=False) -> None:
 
 def get_directories_from_path(path: str) -> str:
     """
-    Remove the filename from the path.
+    Remove the filename from the path and return the directory path.
+
+    Args:
+        path (str): The full path from which to extract the directory path.
+
+    Returns:
+        str: The directory path without the filename.
     """
     return os.path.dirname(path)
 
@@ -28,5 +42,11 @@ def get_directories_from_path(path: str) -> str:
 def get_filename_from_path(path: str) -> str:
     """
     Extract the filename from the given path.
+
+    Args:
+        path (str): The full path from which to extract the filename.
+
+    Returns:
+        str: The filename extracted from the path.
     """
     return os.path.basename(path)
