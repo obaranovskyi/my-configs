12123ee515f02fcd29d172b932f90d31e2c01edd
olegbaranovskyi <oleh.baranovskyi.dev.acc@gmail.com>
2025-11-27 17:26:25 +0200

    Auto git push ...

--- neovim/plugins/ai/copilot.lua
+++ neovim/plugins/ai/copilot.lua
@@ -1,21 +1,10 @@
--- More info: https://github.com/github/copilot.vim/blob/49e0348bfb913fae63ca5ddb987a8bccd193da86/doc/copilot.txt#L146
 return {
 	"github/copilot.vim",
+	event = "InsertEnter",
 	config = function()
-		require("copilot").setup()
-
 		vim.keymap.set({ "i" }, "<C-p>", "<Plug>(copilot-previous)", { silent = true })
 		vim.keymap.set({ "i" }, "<C-n>", "<Plug>(copilot-next)", { silent = true })
 		vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)", { silent = true })
-
-		-- The keymaps that are under testing
-		--
-		-- If you want to use other key than Tab
-		-- vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
-		-- 	expr = true,
-		-- 	replace_keycodes = false,
-		-- })
-		-- vim.g.copilot_no_tab_map = true
 	end,
 }
 
