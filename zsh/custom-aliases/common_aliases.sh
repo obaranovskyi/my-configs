a44749fbf1d20a8e327ac91a515c20d8af22cbf1
olegbaranovskyi <oleh.baranovskyi.dev.acc@gmail.com>
2026-04-28 06:04:51 +0300

    Auto git push ...

--- zsh/custom-aliases/common_aliases.sh
+++ zsh/custom-aliases/common_aliases.sh
@@ -31,17 +31,22 @@ fi
 
 function source-all() {
 	source $HOME/.zshrc;
-	echo " ----------------------- "
-	echo "| - source zsh: done    |"
+	echo " ------------------------- "
+	echo "| - source zsh: done      |"
 
   # setup ranger configs
   mkdir -p ~/.config/ranger
   cat ~/my-configs/ranger/rc.conf > ~/.config/ranger/rc.conf
-	echo "| - source ranger: done |"
+	echo "| - source ranger: done   |"
 
+  if [[ $(uname -s) == "Darwin" ]]; then
+    cat ~/my-configs/amethyst/.amethyst.yml > ~/.amethyst.yml
+    echo "| - source amethyst: done |"
+
+  fi
 	tmux source $HOME/.tmux.conf;
-	echo "| - source tmux: done   |"
-	echo " ----------------------- "
+	echo "| - source tmux: done     |"
+	echo " ------------------------- "
 }
 
 # translate-shell
